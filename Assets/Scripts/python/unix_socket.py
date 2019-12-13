73f3919025858ed49725b9f31a20e25b38e39f73
Ryan Wickman <rwickman@memphis.edu>
2019-12-13 16:56:38 -0600

    Update unix_socket.py

--- Assets/Scripts/python/unix_socket.py
+++ Assets/Scripts/python/unix_socket.py
@@ -1,16 +1,16 @@
 import socket, sys, os
 
-server_adr = "./ai_controller2"
-
+server_adr = "./ai_controller"
 with socket.socket(socket.AF_UNIX, socket.SOCK_STREAM) as s:
     s.bind(server_adr)
     s.listen(1)
     print("Starting accepting")
-    conn, client_adr = s.accept()
-    with conn:
-        print("Connected by", client_adr)
-        while True:
-            data = conn.recv(1024)
-            if not data:
-                break
-            print(data)
+    while True:
+        conn, client_adr = s.accept()
+        with conn:
+            print("Connected by", client_adr)
+            while True:
+                data = conn.recv(1024)
+                if not data:
+                    break
+                print(data)
