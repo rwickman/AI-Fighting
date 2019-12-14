8d8e38b4cdd3e68bf2ffe9987f33d07bed2096d0
Ryan Wickman <rwickman@memphis.edu>
2019-12-14 16:49:23 -0600

    Add AI navigation and added script from other project

---
+++ Assets/Scripts/Sword.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sword : MonoBehaviour
+{
+    public string swordAnimName = "SwordAttack";
+    public float hitRate = 0.5f;
+    public float hitForce = 1.4f;
+    public int hitDamage = 2;
+
+    private Animator m_anim;
+    private Collider m_coll;
+    private float elapsedTime = 0f, animElapsedTime = 0f;
+    private float hitAnimTime;
+    private HashSet<int> objectsHitAlready = new HashSet<int>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_anim = gameObject.GetComponent<Animator>();
+        m_coll = gameObject.GetComponent<Collider>();
+
+        RuntimeAnimatorController ac = m_anim.runtimeAnimatorController;
+        for (int i = 0; i < ac.animationClips.Length; i++) {
+            if (ac.animationClips[i].name == swordAnimName) {
+                hitAnimTime = ac.animationClips[i].length;
+            }
+        }   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        elapsedTime += Time.deltaTime;
+        animElapsedTime += Time.deltaTime;
+
+        if (animElapsedTime > hitAnimTime && objectsHitAlready.Count > 0) {
+            objectsHitAlready = new HashSet<int>();
+        }
+    }
+
+    public void Attack()
+    {
+        if(elapsedTime >= hitRate) {
+            m_anim.SetTrigger("isAttacking");
+            elapsedTime = 0f;
+            animElapsedTime = 0f;
+        }
+    }
+
+
+    private void OnTriggerStay(Collider other) {
+         print("TRIGGERING");
+        Health otherHealth = other.gameObject.GetComponent<Health>();
+        
+        if(otherHealth != null && 
+           animElapsedTime < hitAnimTime && 
+           !m_coll.transform.IsChildOf(other.transform) &&
+           !objectsHitAlready.Contains(other.GetInstanceID())) {
+            
+            objectsHitAlready.Add(other.GetInstanceID());
+
+            Vector3 dir = other.transform.position - transform.position;
+            Rigidbody otherRb = other.gameObject.GetComponent<Rigidbody>();
+            other.gameObject.GetComponent<UnityEngine.AI.NavMeshAgent>().enabled = false;
+            otherRb.isKinematic = false;
+            otherRb.AddForce(dir.normalized * hitForce,  ForceMode.Impulse);
+
+        }
+    }
+
+}
