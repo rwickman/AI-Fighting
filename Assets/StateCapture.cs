59e6e5852e4232f5d8483ebd10a6a710e69c8778
Ryan <rwickman@memphis.edu>
2020-01-09 14:01:05 -0600

    Add more raycasts

--- Assets/StateCapture.cs
+++ Assets/StateCapture.cs
@@ -32,18 +32,24 @@ public class StateCapture : MonoBehaviour
             angle += 2 * Mathf.PI / numRaycast;
 
             Vector3 localForward = transform.TransformDirection(transform.forward);
-            Vector3 dir = new Vector3(localForward.x + x_angle, 0, localForward.y + y_angle); // Used for horizontally around body (think hoola hoop)
+            Vector3 dir = new Vector3(localForward.x + x_angle, 0, localForward.y + y_angle); // Used for horizontally around body (think hoola hoop) x dir
             
-            Vector3 dir2 = new Vector3(localForward.x + x_angle, localForward.y + y_angle, 0); // Verticall aroudn body (think door maybe?)
+            Vector3 dir2 = new Vector3(localForward.x + x_angle, localForward.y + y_angle, 0); // Verticall aroudn body y dir
 
+            Vector3 dir3 = new Vector3(0, localForward.y + y_angle, localForward.x + x_angle); // Verticall aroudn body y dir
             
-            
-            Vector3 dir3 = Quaternion.AngleAxis(45, Vector3.right) * dir;
+            // The angled raycast directions 
+            Vector3 dir4 = Quaternion.AngleAxis(45, Vector3.right) * dir;
 
-            Vector3 dir4 = Quaternion.AngleAxis(-45, Vector3.right) * dir;
+            Vector3 dir5 = Quaternion.AngleAxis(-45, Vector3.right) * dir;
+            
+            Vector3 dir6= Quaternion.AngleAxis(-90, Vector3.up) * dir5;
+
+            Vector3 dir7 = Quaternion.AngleAxis(90, Vector3.up) * dir5;
+
 
             RaycastHit hit;
-            
+
             Debug.DrawRay(transform.position, dir, Color.magenta);
             if (Physics.Raycast(transform.position, dir, out hit, Mathf.Infinity, layerMask))
             {
@@ -67,6 +73,27 @@ public class StateCapture : MonoBehaviour
             {
                 Debug.Log("HIT: " + hit.collider.gameObject.name);
             }
+
+            Debug.DrawRay(transform.position, dir5, Color.magenta);
+            if (Physics.Raycast(transform.position, dir5, out hit, Mathf.Infinity, layerMask))
+            {
+                Debug.Log("HIT: " + hit.collider.gameObject.name);
+            }
+
+            Debug.DrawRay(transform.position, dir6, Color.magenta);
+            if (Physics.Raycast(transform.position, dir6, out hit, Mathf.Infinity, layerMask))
+            {
+                Debug.Log("HIT: " + hit.collider.gameObject.name);
+            }
+
+            Debug.DrawRay(transform.position, dir7, Color.magenta);
+            if (Physics.Raycast(transform.position, dir7, out hit, Mathf.Infinity, layerMask))
+            {
+                Debug.Log("HIT: " + hit.collider.gameObject.name);
+            }
+
+
+
         }
     }
 }
