5360b224922de2b288922fcfa4fe4fcbd2e88fd9
Ryan <rwickman@memphis.edu>
2020-01-10 20:05:05 -0600

    Removed duplicate code and remove duplicate rays

--- Assets/StateCapture.cs
+++ Assets/StateCapture.cs
@@ -32,68 +32,56 @@ public class StateCapture : MonoBehaviour
             angle += 2 * Mathf.PI / numRaycast;
 
             Vector3 localForward = transform.TransformDirection(transform.forward);
-            Vector3 dir = new Vector3(localForward.x + x_angle, 0, localForward.y + y_angle); // Used for horizontally around body (think hoola hoop) x dir
-            
-            Vector3 dir2 = new Vector3(localForward.x + x_angle, localForward.y + y_angle, 0); // Verticall aroudn body y dir
-
-            Vector3 dir3 = new Vector3(0, localForward.y + y_angle, localForward.x + x_angle); // Verticall aroudn body y dir
-            
+            Vector3 dir1 = new Vector3(localForward.x + x_angle, 0, localForward.y + y_angle); // Used for horizontally around body (think hoola hoop)
+            Vector3 dir2 = new Vector3(localForward.x + x_angle, localForward.y + y_angle, 0); // Verticall aroudn body
+            Vector3 dir3 = new Vector3(0, localForward.y + y_angle, localForward.x + x_angle); 
             // The angled raycast directions 
-            Vector3 dir4 = Quaternion.AngleAxis(45, Vector3.right) * dir;
-
-            Vector3 dir5 = Quaternion.AngleAxis(-45, Vector3.right) * dir;
-            
+            Vector3 dir4 = Quaternion.AngleAxis(45, Vector3.right) * dir1;
+            Vector3 dir5 = Quaternion.AngleAxis(-45, Vector3.right) * dir1;
             Vector3 dir6= Quaternion.AngleAxis(-90, Vector3.up) * dir5;
-
             Vector3 dir7 = Quaternion.AngleAxis(90, Vector3.up) * dir5;
 
+            // Remove duplicate rays (there is probably a better way to do this but oh well)
+            Vector3[] dirs = { dir1, dir2, dir3, dir4, dir5, dir6, dir7 };
 
-            RaycastHit hit;
-
-            Debug.DrawRay(transform.position, dir, Color.magenta);
-            if (Physics.Raycast(transform.position, dir, out hit, Mathf.Infinity, layerMask))
+            List<Vector3> dirs_non_duplicates = new List<Vector3>();
+            
+            for (int j = 0; j < dirs.Length; j++)
             {
-                Debug.Log("HIT: " + hit.collider.gameObject.name);
+                bool isDuplicate = false;
+                foreach (Vector3 dir in dirs_non_duplicates)
+                {
+                    if (dirs[j].x == dir.x && dirs[j].y == dir.y && dirs[j].z == dir.z)
+                    {
+                        isDuplicate = true;
+                        break;
+                    }
+                }
+
+                if (!isDuplicate)
+                {
+                    dirs_non_duplicates.Add(dirs[j]);
+                }
             }
 
-            Debug.DrawRay(transform.position, dir2, Color.magenta);
-            if (Physics.Raycast(transform.position, dir2, out hit, Mathf.Infinity, layerMask))
+            // Raycast single ray
+            foreach (Vector3 dir in dirs_non_duplicates)
             {
-                Debug.Log("HIT: " + hit.collider.gameObject.name);
-            }
-
-            Debug.DrawRay(transform.position, dir3, Color.magenta);
-            if (Physics.Raycast(transform.position, dir3, out hit, Mathf.Infinity, layerMask))
-            {
-                Debug.Log("HIT: " + hit.collider.gameObject.name);
-            }
-
-            Debug.DrawRay(transform.position, dir4, Color.magenta);
-            if (Physics.Raycast(transform.position, dir4, out hit, Mathf.Infinity, layerMask))
-            {
-                Debug.Log("HIT: " + hit.collider.gameObject.name);
-            }
-
-            Debug.DrawRay(transform.position, dir5, Color.magenta);
-            if (Physics.Raycast(transform.position, dir5, out hit, Mathf.Infinity, layerMask))
-            {
-                Debug.Log("HIT: " + hit.collider.gameObject.name);
-            }
-
-            Debug.DrawRay(transform.position, dir6, Color.magenta);
-            if (Physics.Raycast(transform.position, dir6, out hit, Mathf.Infinity, layerMask))
-            {
-                Debug.Log("HIT: " + hit.collider.gameObject.name);
-            }
-
-            Debug.DrawRay(transform.position, dir7, Color.magenta);
-            if (Physics.Raycast(transform.position, dir7, out hit, Mathf.Infinity, layerMask))
-            {
-                Debug.Log("HIT: " + hit.collider.gameObject.name);
+                RaycastSubState(dir);
             }
 
 
+        }
+    }
 
+    void RaycastSubState(Vector3 dir)
+    {
+        RaycastHit hit;
+
+        Debug.DrawRay(transform.position, dir, Color.magenta);
+        if (Physics.Raycast(transform.position, dir, out hit, Mathf.Infinity, layerMask))
+        {
+            Debug.Log("HIT: " + hit.collider.gameObject.name);
         }
     }
 }
