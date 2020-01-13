2892b7d8b0907498aace27d9b5ec7da97591cf3b
Ryan <rwickman@memphis.edu>
2020-01-13 13:00:48 -0600

    Fix backend name

--- Assets/Scripts/Agent/python/ppo_model.py
+++ Assets/Scripts/Agent/python/ppo_model.py
@@ -1,6 +1,5 @@
 import tensorflow as tf
 import numpy as np
-from.tf.keras import backend as K
 
 
 class PPOModel:
@@ -16,15 +15,15 @@ class PPOModel:
     
     def ppo_loss_continuous(self, advantage, old_prediction):
         def loss(y_true, y_pred):
-            denom = K.sqrt(2 * np.pi * self.var)
-            prob_num = K.exp(- K.square(y_true - y_pred) / (2 * self.var))
-            old_prob_num = K.exp(- K.square(y_true - old_prediction) / (2 * self.var))
+            denom = tf.keras.backend.sqrt(2 * np.pi * self.var)
+            prob_num = tf.keras.backend.exp(- tf.keras.backend.square(y_true - y_pred) / (2 * self.var))
+            old_prob_num = tf.keras.backend.exp(- tf.keras.backend.square(y_true - old_prediction) / (2 * self.var))
 
             prob = prob_num/denom
             old_prob = old_prob_num/denom
             r = prob/(old_prob + 1e-10)
 
-            return -K.mean(K.minimum(r * advantage, K.clip(r, min_value=1 - self.epsilon_clip, max_value=1 + self.epsilon_clip) * advantage))
+            return -tf.keras.backend.mean(tf.keras.backend.minimum(r * advantage, tf.keras.backend.clip(r, min_value=1 - self.epsilon_clip, max_value=1 + self.epsilon_clip) * advantage))
         return loss
 
     
@@ -37,21 +36,22 @@ class PPOModel:
         for _ in range(self.num_hidden_layers - 1):
             x = tf.keras.layers.Dense(self.hidden_size, activation="relu")(x)
         
-        out_actor = tf.keras.layers.Dense(self.num_actions, activation="tanh", kernel_initializer="normal")(x)
+        out_actor = tf.keras.layers.Dense(self.num_actions, activation="tanh")(x)
         out_critic = tf.keras.layers.Dense(1)(x)
 
         self.actor = tf.keras.models.Model(inputs=[inputs, advantage, old_prediction], outputs=[out_actor])
         self.critic = tf.keras.models.Model(inputs=[inputs], outputs=[out_critic])
 
         self.actor.compile(
-            optimizer=tf.keras.optimizer.Adam(lr=self.lose_rate),
+            optimizer=tf.keras.optimizers.Adam(lr=self.lose_rate),
             loss=[self.ppo_loss_continuous(
                 advantage=advantage,
                 old_prediction=old_prediction)]
             )
-        self.critic.compile(optimzer=tf.keras.optimizer.Adam(lr=self.lose_rate), loss="mse")
+        self.critic.compile(optimzer=tf.keras.optimizers.Adam(lr=self.lose_rate), loss="mse")
         
         self.actor.summary()
         self.critic.summary()
         
-
+ppo_model = PPOModel(10)
+ppo_model.build_actor_and_critic()
