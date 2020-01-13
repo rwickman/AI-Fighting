56f3a9f41b2e372d4222df65ff2716ea66247007
Ryan <rwickman@memphis.edu>
2020-01-13 12:36:10 -0600

    Add loss function and bulding PPO models

--- Assets/Scripts/Agent/python/ppo_model.py
+++ Assets/Scripts/Agent/python/ppo_model.py
@@ -1,21 +1,57 @@
 import tensorflow as tf
 import numpy as np
+from.tf.keras import backend as K
 
 
 class PPOModel:
-    def __init__(self, num_states, hidden_size=256, num_hidden_layers = 5):
+    def __init__(self, num_states, num_actions = 11, hidden_size=256, num_hidden_layers = 5, epsilon_clip=0.2 ):
         self.num_states = num_states
+        self.num_actions = num_actions
         self.hidden_size = hidden_size
         self.num_hidden_layers = num_hidden_layers
-        pass
+        self.lose_rate = 1e-4
+        self.var = 1
+        self.epsilon_clip = epsilon_clip
+
     
-    def build_actor(self):
+    def ppo_loss_continuous(self, advantage, old_prediction):
+        def loss(y_true, y_pred):
+            denom = K.sqrt(2 * np.pi * self.var)
+            prob_num = K.exp(- K.square(y_true - y_pred) / (2 * self.var))
+            old_prob_num = K.exp(- K.square(y_true - old_prediction) / (2 * self.var))
+
+            prob = prob_num/denom
+            old_prob = old_prob_num/denom
+            r = prob/(old_prob + 1e-10)
+
+            return -K.mean(K.minimum(r * advantage, K.clip(r, min_value=1 - self.epsilon_clip, max_value=1 + self.epsilon_clip) * advantage))
+        return loss
+
+    
+    def build_actor_and_critic(self):
         inputs = tf.keras.Input(shape=(self.num_states,))
-        for _ in range(self.num_hidden_layers):
+        advantage = tf.keras.Input(shape=(1,))
+        old_prediction = tf.keras.Input(shape=(self.num_actions,))
+        
+        x = tf.keras.layers.Dense(self.hidden_size, activation="relu")(inputs)
+        for _ in range(self.num_hidden_layers - 1):
+            x = tf.keras.layers.Dense(self.hidden_size, activation="relu")(x)
+        
+        out_actor = tf.keras.layers.Dense(self.num_actions, activation="tanh", kernel_initializer="normal")(x)
+        out_critic = tf.keras.layers.Dense(1)(x)
 
-            tf.keras.layers.Dense
-        self.actor = tf.keras.models.Sequential()
-        pass
+        self.actor = tf.keras.models.Model(inputs=[inputs, advantage, old_prediction], outputs=[out_actor])
+        self.critic = tf.keras.models.Model(inputs=[inputs], outputs=[out_critic])
 
-    def build_critc(self):
+        self.actor.compile(
+            optimizer=tf.keras.optimizer.Adam(lr=self.lose_rate),
+            loss=[self.ppo_loss_continuous(
+                advantage=advantage,
+                old_prediction=old_prediction)]
+            )
+        self.critic.compile(optimzer=tf.keras.optimizer.Adam(lr=self.lose_rate), loss="mse")
+        
+        self.actor.summary()
+        self.critic.summary()
+        
 
